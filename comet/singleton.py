2525fac547c5ac365f808d295d851b895326b3b9
Sadegh Yazdani <silverstar10@gmail.com>
2022-10-31 13:59:17 +0330

    feat: add two Metaclass for singleton

---
+++ comet/singleton.py
@@ -0,0 +1,36 @@
+import threading
+
+
+class SimpleSingletonMeta(type):
+    """
+    The Singleton class can be implemented in different ways in Python. Some
+    possible methods include: base class, decorator, metaclass. We will use the
+    metaclass because it is best suited for this purpose.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class ThreadSafeSingletonMeta(type):
+    _instances = {}
+    _singleton_lock = threading.Lock()
+
+    def __call__(cls, *args, **kwargs):
+        # double-checked locking pattern (https://en.wikipedia.org/wiki/Double-checked_locking)
+        if cls not in cls._instances:
+            with cls._singleton_lock:
+                if cls not in cls._instances:
+                    cls._instances[cls] = super(__class__, cls).__call__(
+                        *args, **kwargs
+                    )
+        return cls._instances[cls]
